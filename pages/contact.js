c376399027a6f5f3810745b2422f17787088f2d1
JakubVarinsky <Hello@varo.design>
2021-01-06 17:27:59 +1100

    Portfolio split screen

--- pages/contact.js
+++ pages/contact.js
@@ -4,18 +4,7 @@ import Head from 'next/head'
 export default function Contact() {
   return (
     <div className="container">
-    <div className="split left">
-      <div className="centered">
-        <h2>Jane Flex</h2>
-        <p>Some text.</p>
-      </div>
+      <h2>Contact</h2>
     </div>
-    <div className="split right">
-      <div className="centered">
-        <h2>John Doe</h2>
-        <p>Some text here too.</p>
-      </div>
-    </div>
-</div>
   )
 }
