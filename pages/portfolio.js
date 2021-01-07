b85794dde756d5559f56d1c63acc1a944282bd79
JakubVarinsky <Hello@varo.design>
2021-01-07 17:24:52 +1100

    Portfolio background image scaling skewing

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,15 +1,22 @@
+import React, { useState } from 'react';
+
 export default function Portfolio() {
+  const [hovered, setHovered] = useState(false);
+  const toggleHover = () => setHovered(!hovered);
+
   return (
     <div className="container">
     <div className="split left">
+      <div className={hovered ? 'background-image active' : 'background-image'} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
       <div className="centered">
-        <h2>Jane Flex</h2>
+        <h2>PHOTOGRAPHY</h2>
         <p>Some text.</p>
+      </div>
       </div>
     </div>
     <div className="split right">
       <div className="centered">
-        <h2>John Doe</h2>
+        <h2>WEB DEVELOPMENT</h2>
         <p>Some text here too.</p>
       </div>
     </div>
