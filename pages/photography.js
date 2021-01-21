b660fb431409e4865805b7edf21ec35c6cd60bd2
Jakub <hello@varo.design>
2021-01-21 22:09:22 +1100

    Photography sorting v.1

--- pages/photography.js
+++ pages/photography.js
@@ -2,15 +2,50 @@ import Head from 'next/document';
 import React, { useState } from 'react';
 
 export default function Portfolio() {
-  const [className, setClassName] = useState("")
-  const myClick = () => {
-    setClassName("some-class")
+  const show = "show"
+  const [firstName, setFirstName] = useState(show)
+  const [secondName, setSecondName] = useState(show)
+  const [thirdName, setThirdName] = useState(show)
+
+  const allClass = () => {
+    setFirstName(show)
+    setSecondName(show)
+    setThirdName(show)
+  }
+  const firstClass = () => {
+    if (secondName || thirdName  === show){
+      setFirstName(show)
+      setSecondName("")
+      setThirdName("")
+    }
+    else setFirstName(show)
+  }
+  const secondClass = () => {
+      if (firstName || thirdName === show){
+        setFirstName("")
+        setSecondName(show)
+        setThirdName("")
+      }
+      else setSecondName(show)
+  }
+  const thirdClass = () => {
+    if (firstName || secondName === show){
+      setFirstName("")
+      setSecondName("")
+      setThirdName(show)
+    }
+    else setThirdName(show)
 }
 
   return (
     <div className="container">
-    <button onClick={myClick}>Click me</button>
-    <div className={className}>Classname gets changes</div>
+    <button onClick={allClass}>all</button>
+    <button onClick={firstClass}>first</button>
+    <button onClick={secondClass}>second</button>
+    <button onClick={thirdClass}>third</button>
+    <div className={`img ${firstName}`}>11111111111111</div>
+    <div className={`img ${secondName}`}>22222222222222</div>
+    <div className={`img ${thirdName}`}>33333333333333</div>
   </div>
   )
 }
