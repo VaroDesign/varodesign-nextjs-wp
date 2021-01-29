1c796ca640f87078f0dfa56d6abfe0fb4ccf262f
JakubVarinsky <Hello@varo.design>
2021-01-29 17:47:56 +1100

    Photography Gallery v 1.1

--- components/portfolio/photos.js
+++ components/portfolio/photos.js
@@ -12,36 +12,62 @@ class Photos extends Component {
       {
         title: "Mustang",
         type: "cars",
+        url: "/img/5d9b5bff52887931e8497a36.png"
       },
       {
         title: "Orange",
         type: "fruits",
+        url: "/img/bugatti-chiron-pur-sport-106-1582836604.jpg"
       },
       {
         title: "Suzuki",
         type: "cars",
+        url: "/img/5d9b5bff52887931e8497a36.png"
       },
       {
         title: "Fish",
         type: "animal",
+        url: "/img/south-china-tiger.adapt.1900.1.jpg"
       },
       {
         title: "Toyota",
         type: "cars",
+        url: "/img/bugatti-chiron-pur-sport-106-1582836604.jpg"
       },
       {
-        title: "Pinapple",
+        title: "qweqqq",
         type: "fruits",
+        url: "/img/healthy-fruits-1296x728.jpg"
       },
       {
-        title: "Dog",
+        title: "dvs",
         type: "animal",
+        url: "/img/subordinate-meerkat-pack.jpg"
+      },
+      {
+        title: "reg",
+        type: "cars",
+        url: "/img/5d9b5bff52887931e8497a36.png"
+      },
+      {
+        title: "qwe",
+        type: "fruits",
+        url: "/img/bugatti-chiron-pur-sport-106-1582836604.jpg"
+      },
+      {
+        title: "asd",
+        type: "cars",
+        url: "/img/5d9b5bff52887931e8497a36.png"
+      },
+      {
+        title: "sad",
+        type: "animal",
+        url: "/img/south-china-tiger.adapt.1900.1.jpg"
       },
      ],
       filterCategory: []
     }
   }
-
   componentDidMount(){
     this.setState({
       filterCategory: this.state.categories
@@ -64,7 +90,7 @@ class Photos extends Component {
       this.state.filterCategory.map(category =>
         <CSSTransition timeout={600} classNames="fade" key={category.title}>
           <div className="column">
-            <div className={"photo-img"}>{category.title}</div>
+            <img className={"photo-img"} src={category.url}></img>
           </div>
         </CSSTransition>
       )}
