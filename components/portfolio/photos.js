172aa8e88c79a478c4532eaea4c941a5580fd3f5
JakubVarinsky <jakub.varinsky@ping-works.com.au>
2021-02-16 11:17:38 +1100

    Photography Gallery v 1.2

--- components/portfolio/photos.js
+++ components/portfolio/photos.js
@@ -8,7 +8,7 @@ import { Button } from '../../lib/photo';
 class Photos extends Component {
   constructor() {
     super();
-    this.state = { categories: [
+    this.state = { left: [
       {
         title: "Mustang",
         type: "cars",
@@ -65,46 +65,127 @@ class Photos extends Component {
         url: "/img/south-china-tiger.adapt.1900.1.jpg"
       },
      ],
-      filterCategory: []
+      middle: [
+        {
+          title: "asd",
+          type: "cars",
+          url: "/img/5d9b5bff52887931e8497a36.png"
+        },
+        {
+          title: "sad",
+          type: "animal",
+          url: "/img/south-china-tiger.adapt.1900.1.jpg"
+        },
+        {
+          title: "dvs",
+          type: "animal",
+          url: "/img/subordinate-meerkat-pack.jpg"
+        },
+        {
+          title: "qwe",
+          type: "fruits",
+          url: "/img/bugatti-chiron-pur-sport-106-1582836604.jpg"
+        },
+      ],
+      right: [
+        {
+          title: "dvs",
+          type: "animal",
+          url: "/img/subordinate-meerkat-pack.jpg"
+        },
+        {
+          title: "sad",
+          type: "animal",
+          url: "/img/south-china-tiger.adapt.1900.1.jpg"
+        },
+        {
+          title: "qwe",
+          type: "fruits",
+          url: "/img/bugatti-chiron-pur-sport-106-1582836604.jpg"
+        },
+        {
+          title: "asd",
+          type: "cars",
+          url: "/img/5d9b5bff52887931e8497a36.png"
+        },
+      ],
+      
+      filterLeft: [],
+      filterMiddle: [],
+      filterRight: [],
     }
   }
   componentDidMount(){
     this.setState({
-      filterCategory: this.state.categories
+      filterLeft: this.state.left,
+      filterMiddle: this.state.middle,
+      filterRight: this.state.right
     })
   }
 
   handleClick = event => {
     const byType = event.target.value
-    let filterCategory = []
+    let filterLeft = []
+    let filterMiddle = []
+    let filterRight = []
     if(event.target.value === 'all'){
-      filterCategory = this.state.categories
+      filterLeft = this.state.left
+      filterMiddle = this.state.middle
+      filterRight = this.state.right
     } else{
-      filterCategory = this.state.categories.filter(category => category.type === byType)
+      filterLeft = this.state.left.filter(category => category.type === byType)
+      filterMiddle = this.state.middle.filter(category => category.type === byType)
+      filterRight = this.state.right.filter(category => category.type === byType)
     }
-    this.setState({filterCategory: filterCategory})
+    this.setState({
+      filterLeft: filterLeft,
+      filterMiddle: filterMiddle,
+      filterRight: filterRight,
+    })
   }
 
   render() {
-    const renderAll = <TransitionGroup component={null}>{
-      this.state.filterCategory.map(category =>
+    const renderLeft = <TransitionGroup component={null}>{
+      this.state.filterLeft.map(category =>
         <CSSTransition timeout={600} classNames="fade" key={category.title}>
-          <div className="column">
             <img className={"photo-img"} src={category.url}></img>
-          </div>
         </CSSTransition>
       )}
       </TransitionGroup>
       const renderButtons = Button.map((button)=>
         <button key={button.title} value={button.value} onClick={this.handleClick}>{button.title}</button>
       )
+      const renderMiddle =
+        this.state.filterMiddle.map(category =>
+          <div key={category.title}>
+            <img className={"photo-img"} src={category.url}></img>
+          </div>      
+        )
+
+        const renderRight = <TransitionGroup component={null}>{
+          this.state.filterRight.map(category =>
+            <CSSTransition timeout={600} classNames="fade" key={category.title}>
+                <img className={"photo-img"} src={category.url}></img>
+            </CSSTransition>
+          )}
+          </TransitionGroup>
       
     return (
         <div>
           <div className="buttons">
             {renderButtons}
           </div>
-            {renderAll}
+          <div className="row">
+              <div className="column">
+                {renderLeft}
+              </div>
+              <div className="column">
+                {renderMiddle}
+              </div>
+              <div className="column">
+                {renderRight}
+              </div>
+            </div>
         </div>
     );
   }
