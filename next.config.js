3721c5cca440d04d413db67bb6b6a834bda51deb
JakubVarinsky <jakub.varinsky@ping-works.com.au>
2021-05-18 12:02:38 +1000

    htaccess test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/html/about.html',
-        permanent: true,
-      },
-    ]
-  },
-}
-
-module.exports = {
   async rewrites() {
     return [
       {
-        source: '/html/about.html',
+        source: '/public/html/about.html',
         destination: '/about',
       },
     ]
   },
-}
+};
